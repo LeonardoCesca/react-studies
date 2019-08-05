695d06b84a08b101284aefe2a8d1a177addf8b0c
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-04 21:59:45 -0300

    ADD axios into Generos to push data api

--- minhas-series/src/Generos.js
+++ minhas-series/src/Generos.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 const Generos = () => {
-    return <h1>Generos</h1>
+    const [data, setData] = useState([])
+    useEffect(() => {
+        axios.get('/api/genres')
+        .then(res => {
+            setData(res.data.data);
+        })
+    }, [])
+    return (
+    <div>
+        <h1>Generos</h1>
+        <pre>{JSON.stringify(data)}</pre>
+    </div>
+    )
 }
 export default Generos
