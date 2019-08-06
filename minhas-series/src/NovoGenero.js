1b26186067da6f0ef5063f7917ac705f13fb9e22
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-05 23:28:30 -0300

    Update new genre component

--- minhas-series/src/NovoGenero.js
+++ minhas-series/src/NovoGenero.js
@@ -5,8 +5,8 @@ const NovoGenero = () => {
         <div className='container'>
             <h1>Novo Genero</h1>
             <form>
-                <div class='form-group'>
-                    <label for='name'>Nome </label>
+                <div className='form-group'>
+                    <label> Nome </label>
                     <input type='text' className='form-control' id='name' placeholder='Nome do Genero' />
                 </div>
             </form>
