acbfbbdfdfee67ef042bab05bfedf2fd212f0805
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-06 06:57:19 -0300

    ADD reactive input

--- minhas-series/src/NovoGenero.js
+++ minhas-series/src/NovoGenero.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 
 const NovoGenero = () => {
+    const [name, setName] = useState('')
+    const onChange = evt => {
+        setName(evt.target.value);
+    }
     return (
         <div className='container'>
             <h1>Novo Genero</h1>
             <form>
                 <div className='form-group'>
                     <label> Nome </label>
-                    <input type='text' className='form-control' id='name' placeholder='Nome do Genero' />
+                    <input type='text' value={name} onChange={onChange} className='form-control' id='name' placeholder='Nome do Genero' />
                 </div>
+                <button type='button' className='btn btn-primary'>Salvar</button>
             </form>
         </div>
     )
