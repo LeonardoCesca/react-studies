81f87ca6eab6dd33d90fd4563623fe1058ba651c
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-05 22:58:04 -0300

    ADD renderizaLinha function

--- minhas-series/src/Generos.js
+++ minhas-series/src/Generos.js
@@ -11,10 +11,10 @@ const Generos = () => {
 
     const renderizaLinha = record => {
         return (
-        <tr>
-            <th scope='row'>1</th>
+        <tr key={record.id}>
+            <th scope='row'>{record.id}</th>
             <td>{record.name}</td>
-            <td></td>
+            <td><button>+</button></td>
         </tr>
         )
     }
