ed76a232fa101715f697671182a088bb2018a5b8
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-06 07:11:09 -0300

    ADD save method and import Redirect react-router-dom

--- minhas-series/src/NovoGenero.js
+++ minhas-series/src/NovoGenero.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 const NovoGenero = () => {
     const [name, setName] = useState('')
+    const [success, setSuccess] = useState(false)
     const onChange = evt => {
         setName(evt.target.value);
+    }
+    const save = () => {
+        axios.post('/api/genres', {
+            name: name
+        })
+        .then(res => {
+            setSuccess(true);
+        })
+    }
+    if (success) {
+        return <Redirect to='/generos'></Redirect>
     }
     return (
         <div className='container'>
@@ -14,7 +27,7 @@ const NovoGenero = () => {
                     <label> Nome </label>
                     <input type='text' value={name} onChange={onChange} className='form-control' id='name' placeholder='Nome do Genero' />
                 </div>
-                <button type='button' className='btn btn-primary'>Salvar</button>
+                <button type='button' onClick={save} className='btn btn-primary'>Salvar</button>
             </form>
         </div>
     )
