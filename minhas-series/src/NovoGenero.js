7f39f708f3bfe2c090770e8f3f8248059c5ff55c
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-05 23:16:07 -0300

    ADD NovoGenero component

---
+++ minhas-series/src/NovoGenero.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const NovoGenero = () => {
+    return (
+        <div className='container'>
+            <h1>Novo Genero</h1>
+        </div>
+    )
+}
+
+export default NovoGenero
