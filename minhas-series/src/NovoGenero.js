8c44927b22ad32e3b91ca1ed9d06e033a9521850
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-05 23:21:45 -0300

    ADD bootstrap form

--- minhas-series/src/NovoGenero.js
+++ minhas-series/src/NovoGenero.js
@@ -4,6 +4,13 @@ const NovoGenero = () => {
     return (
         <div className='container'>
             <h1>Novo Genero</h1>
+            <form>
+                <div class='form-group'>
+                    <label for='exampleInputEmail1'>Email address</label>
+                    <input type='email' class='form-control' id='exampleInputEmail1' aria-describedby='emailHelp' placeholder='Enter email' />
+                    <small id='emailHelp' class='form-text text-muted'>We'll never share your email with anyone else.</small>
+                </div>
+            </form>
         </div>
     )
 }
