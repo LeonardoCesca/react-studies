40faea2f1d4bc046f06d38e120727ffe45b70cf5
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-10 17:05:23 -0300

    ADD binding with props

--- exercicios-react/src/componentes/PrimeiroComponente.jsx
+++ exercicios-react/src/componentes/PrimeiroComponente.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
-export default () => {
-    return <h1>Primeiro Componente</h1>
-}
+let ehLegal = true; 
+
+export default (props) =>
+    <div>
+        <h1>{props.valor}</h1>
+        <p>Legal? {ehLegal ? 'Sim' : 'Não'}</p>
+        <p>{Math.random()}</p>
+    </div>
