1a43985a1de6d8cc6b74cbbdf9737b8552b8e87e
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-10 18:35:33 -0300

    ADD call the components and create an alias

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PrimeiroComponente from './componentes/PrimeiroComponente'
+import { CompA, CompB as B} from './componentes/DoisComponente'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
-        <PrimeiroComponente valor='x'/>
+        {<PrimeiroComponente valor='x'/>}
+        <CompA valor="Olá eu sou o componente A"/>
+        <B valor="Olá eu sou componente B" />
     </div>
     , elemento)
