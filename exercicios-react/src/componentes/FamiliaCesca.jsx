6dae190c42785808f20dbfc905682273492a73cc
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-11 22:37:46 -0300

    ADD create new component - FamiliaCesca and create the props

---
+++ exercicios-react/src/componentes/FamiliaCesca.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Membro from './Membro'
+
+export default props => 
+    <div>
+        <Membro nome='Leonardo' sobrenome='Cesca' />
+        <Membro nome='Tais' sobrenome='Cesca' />
+        <Membro nome='Maria' sobrenome='Cesca' />
+        <Membro nome='Olivo' sobrenome='Cesca' />
+    </div>
