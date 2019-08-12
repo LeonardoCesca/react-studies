09854a496787174f081e6c20f25edde1d69d8bed
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-12 15:12:55 -0300

    UPDATE props with spread method

--- exercicios-react/src/componentes/Familia.jsx
+++ exercicios-react/src/componentes/Familia.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default props => 
     <div>
-        {React.cloneElement(props.children, props)}
+        {React.cloneElement(props.children, { ...props })}
         {/* {React.cloneElement(props.children,
             { sobrenome: props.sobrenome })} */}
         {/* {props.children} */}
