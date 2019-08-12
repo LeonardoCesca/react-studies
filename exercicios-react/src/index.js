4036a2e53edd1f4038cb4ddfec0b48ca5858a8da
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-11 22:38:03 -0300

    UPDATE index.js with this lesson

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PrimeiroComponente from './componentes/PrimeiroComponente'
-import { CompA, CompB as B} from './componentes/DoisComponente'
-import MultiElementos from './componentes/MultiElementos'
+import FamiliaCesca from './componentes/FamiliaCesca'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
-        {<PrimeiroComponente valor='x'/>}
-        <CompA valor="Olá eu sou o componente A"/>
-        <B valor="Olá eu sou componente B" />
-        <MultiElementos />
+       <FamiliaCesca />
     </div>
     , elemento)
