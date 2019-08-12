f0972c43659cf77bc0051e701529f8f59ca48738
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-11 23:36:27 -0300

    ADD refatoring the index.js with the new lesson

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import FamiliaCesca from './componentes/FamiliaCesca'
+import Membro from './componentes/Membro'
+import Familia from './componentes/Familia'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
-       <FamiliaCesca />
+       <Familia>
+           <Membro nome='Leonardo' sobrenome='Cesca'/>
+           <Membro nome='Tais' sobrenome='Cesca'/>
+       </Familia>
+       <Familia>
+           <Membro nome='Leandro' sobrenome='Garcia'/>
+           <Membro nome='Eledi' sobrenome='Garcia'/>
+       </Familia>
     </div>
     , elemento)
