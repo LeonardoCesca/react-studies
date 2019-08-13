84864cbcf23b78ad335ff3558f8b3de9004961c2
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-13 11:37:21 -0300

    UPDATE index.js

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Familia from './componentes/Familia'
-import Membro from './componentes/Membro'
+import ComponenteComFuncao from './componentes/ComponenteComFuncao'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
-       <Familia sobrenome='Cesca'>
-           <Membro nome='Leonardo' />
-       </Familia>
+       <ComponenteComFuncao />
     </div>
     , elemento)
