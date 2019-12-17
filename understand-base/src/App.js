bb95c67aced6a25529d618ca8eede81bcb327cae
LeonardoCesca <leonardocescaf@gmail.com>
2019-12-16 22:55:13 -0300

    ADD working with props (with destructuring - bonus by Leo :D )

--- understand-base/src/App.js
+++ understand-base/src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Person />
+      <Person name="Leo" age="22" />
+      <Person name="Vic" age="21" />
+      <Person name="Tiago" age="37" />
     </div>
   );
 }
