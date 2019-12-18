cd594a54e9725f437ca6aa7a83698d107b1bdb4a
LeonardoCesca <leonardocescaf@gmail.com>
2019-12-17 21:26:24 -0300

    ADD switch name button (with Hooks)

--- understand-base/src/App.js
+++ understand-base/src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 function App() {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Tiago', age: 27},
+    ],
+  });
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        { name: 'Maximilian', age: 28},
+        { name: 'Manuela', age: 29},
+        { name: 'Tiag', age: 27},
+      ]
+    });
+  }
+
   return (
     <div className="App">
-      <Person name="Leo" age="22" />
-      <Person name="Vic" age="21" />
-      <Person name="Tiago" age="37" />
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}></Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+      <button onClick={switchNameHandler}>Switch Names</button>
     </div>
   );
 }
--- understand-base/src/Person/Person.js
+++ understand-base/src/Person/Person.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const person = ({name,age}) => {
-    return <p>I'm a {name} and I am {age} years old!</p>
+  return (
+    <div>
+      <p>I'm a {name} and I am {age} years old!</p>
+    </div>
+  );
 };
 
 export default person; 
